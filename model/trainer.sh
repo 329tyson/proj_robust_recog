2721ca3acdb6c11858c02a75eace9436cf5b4a87
Taeyoung Son <ty.son@postech.ac.kr>
2019-08-08 14:41:15 +0900

    training_script:: script can be run as DEBUG mode

--- model/trainer.sh
+++ model/trainer.sh
@@ -1,10 +1,33 @@
-read -p "desc: " desc
+case $1 in
+    "--f")
+        echo "PROCESS IS RUNNING FOREGROUND"
+        front=true
+        ;;
+    "-f")
+        echo "PROCESS IS RUNNING FOREGROUND"
+        front=true
+        ;;
+    *)
+        front=false
+        ;;
+esac
 
 dt=$(date +"D%d_%T")
 logdir=logs
 logfile=${logdir}/${dt}_${desc}.log
-
 touch ${logfile}
-python trainer.py --epochs 100 --logfile ${logfile} &>/dev/null &
+
+cmd="python trainer.py --epochs 100 --logfile ${logfile}"
+
+echo ${front}
+if [ ${front} ]; then
+    ${cmd}
+    rm ${logfile}
+    exit
+fi
+
+read -p "desc: " desc
+cmd &>/dev/null &
+
 
 multitail -n 1000 ${logfile}
